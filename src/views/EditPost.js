64977888e36ba9cf113f36a246798c3dbe200572
Heiry <119796148+HeiryH@users.noreply.github.com>
2023-09-07 13:45:07 +0800

    add try catch on EditPost

--- src/views/EditPost.js
+++ src/views/EditPost.js
@@ -31,13 +31,19 @@ export default function EditPost() {
     if (files?.[0]) {
       data.set('file', files?.[0]);
     }
-    const response = await fetch('https://crud-miniwebproject-7340dd192120.herokuapp.com/post', {
-      method: 'PUT',
-      body: data,
-      credentials: 'include',
-    });
-    if (response.ok) {
-      setRedirect(true);
+
+    try{
+      
+      const response = await fetch('https://crud-miniwebproject-7340dd192120.herokuapp.com/post', {
+        method: 'PUT',
+        body: data,
+        credentials: 'include',
+      });
+      if (response.ok) {
+        setRedirect(true);
+      }
+    }catch(err){
+      alert('error, restart server')
     }
   }
 

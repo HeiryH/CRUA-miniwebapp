71a061823ad2c064f5c96d0b9756150d5e92dd46
Heiry <119796148+HeiryH@users.noreply.github.com>
2023-09-07 13:07:08 +0800

    add try catch on NewPost

--- src/views/NewPost.js
+++ src/views/NewPost.js
@@ -1,47 +1,53 @@
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css';
 import { useState } from 'react';
-import { Navigate} from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import Editor from '../component/Editor';
 
 export default function NewPost() {
-    const [title, setTitle] = useState('')
-    const [summary, setSummary] = useState('')
-    const [content, setContent] = useState('')
-    const [files, setFiles] = useState('')
-    const [redirect, setRedirect] = useState(false);
+  const [title, setTitle] = useState('')
+  const [summary, setSummary] = useState('')
+  const [content, setContent] = useState('')
+  const [files, setFiles] = useState('')
+  const [redirect, setRedirect] = useState(false);
 
-    async function createNewPost(e){
-        const data = new FormData();
-        data.set('title', title);
-        data.set('summary', summary);
-        data.set('content', content);
-        data.set('file', files[0]);
-        e.preventDefault();
-        const response = await fetch('https://crud-miniwebproject-7340dd192120.herokuapp.com/post', {
-          method: 'POST',
-          body: data,
-          credentials: 'include',
-        });
-        if (response.ok) {
-          setRedirect(true);
-        }
+  async function createNewPost(e) {
+    e.preventDefault();
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('file', files[0]);
+
+    try{
+      const response = await fetch('https://crud-miniwebproject-7340dd192120.herokuapp.com/post', {
+        method: 'POST',
+        body: data,
+        credentials: 'include',
+      });
+      if (response.status === 200) {
+        setRedirect(true);
       }
-    
-      if (redirect) {
-        return <Navigate to={'/'} />
-      }
-    return (
-        <div className='max-w-xl m-auto'>
-            <form action="" onSubmit={createNewPost}>
-            <input type="title" placeholder={"Title"} value={title}  onChange={e => setTitle(e.target.value)}/>
-            <input type="summary" placeholder={"Summary"}  value={summary} onChange={e => setSummary(e.target.value)}/>
-            <input type="file" onChange={e => setFiles(e.target.files)}/>
 
-            <Editor onChange={setContent} value={content}/>
+    }catch(err){
+      alert('error, restart server')
+    }
+  }
 
-            <button> Post It </button>
-            </form>
-        </div>
-    )
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
+  return (
+    <div className='max-w-xl m-auto'>
+      <form action="" onSubmit={createNewPost}>
+        <input type="title" placeholder={"Title"} value={title} onChange={e => setTitle(e.target.value)} />
+        <input type="summary" placeholder={"Summary"} value={summary} onChange={e => setSummary(e.target.value)} />
+        <input type="file" onChange={e => setFiles(e.target.files)} />
+
+        <Editor onChange={setContent} value={content} />
+
+        <button> Post It </button>
+      </form>
+    </div>
+  )
 }

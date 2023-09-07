2f39d72815a878729de333cbe62c08b46b1106c2
Helmy Heiry <nws06_01098906@pb.edu.bn>
2023-09-07 18:12:46 +0800

    test change in header

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,7 @@ import { UserContext } from "../UserContext";
 export default function Header() {
   const {userInfo, setUserInfo} = useContext(UserContext)
   useEffect(() => {
-    fetch('http://localhost:5000/profile', {
+    fetch('https://crud-miniwebproject-7340dd192120.herokuapp.com/profile', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
@@ -15,7 +15,7 @@ export default function Header() {
   }, [])
 
   function logOut() {
-    fetch('http://localhost:5000/logout', {
+    fetch('https://crud-miniwebproject-7340dd192120.herokuapp.comhttp://localhost:5000/logout', {
       credentials: 'include',
       method: 'POST'
     })
